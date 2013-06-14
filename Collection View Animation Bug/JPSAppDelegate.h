19a224771e48a5e01e2d1e20a9a6ae590ede30b2
iOctopress <ioctopress@jpsim.com>
2013-06-14 13:27:39 -0700

    Initial Commit

---
+++ Collection View Animation Bug/JPSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JPSAppDelegate.h
+//  Collection View Animation Bug
+//
+//  Created by Jean-Pierre Simard on 6/14/13.
+//  Copyright (c) 2013 JP Simard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JPSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Collection View Animation Bug/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Collection View Animation Bug
+//
+//  Created by Jean-Pierre Simard on 6/14/13.
+//  Copyright (c) 2013 JP Simard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JPSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JPSAppDelegate class]));
+    }
+}

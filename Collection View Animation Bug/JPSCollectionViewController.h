b9e0701df9ef5c6aabf3dbc90b0090be060c3ba7
iOctopress <ioctopress@jpsim.com>
2013-06-14 18:28:05 -0700

    Added main files

---
+++ Collection View Animation Bug/JPSCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  JPSCollectionViewController.h
+//  Collection View Animation Bug
+//
+//  Created by Jean-Pierre Simard on 6/14/13.
+//  Copyright (c) 2013 JP Simard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JPSCollectionViewController : UICollectionViewController
+
+@end
---
+++ Collection View Animation Bug/JPSCollectionViewController.m
@@ -0,0 +1,112 @@
+//
+//  JPSCollectionViewController.m
+//  Collection View Animation Bug
+//
+//  Created by Jean-Pierre Simard on 6/14/13.
+//  Copyright (c) 2013 JP Simard. All rights reserved.
+//
+
+#import "JPSCollectionViewController.h"
+
+#define kCellReuseID        @"Cell"
+#define kWidth              300
+#define kCollapsedHeight    100
+#define kExpandedHeight     300
+#define kNumberOfCells      10
+#define kHackEnabled        TRUE
+#define kContentOffsetKey   @"contentOffset"
+
+@implementation JPSCollectionViewController {
+    NSMutableArray *_cells;
+    BOOL _forceContentOffset;
+    CGPoint _originalOffset;
+}
+
+#pragma mark - Lifecycle
+
+- (id)init {
+    self = [super initWithCollectionViewLayout:[[UICollectionViewFlowLayout alloc] init]];
+    if (self) {
+        _cells = [NSMutableArray arrayWithCapacity:kNumberOfCells];
+        
+        for (int cell_idx = 0; cell_idx < kNumberOfCells; cell_idx++) {
+            [_cells addObject:@(kCollapsedHeight)];
+        }
+        
+        [self.collectionView registerClass:UICollectionViewCell.class forCellWithReuseIdentifier:kCellReuseID];
+        
+        if (kHackEnabled) {
+            [self.collectionView addObserver:self forKeyPath:kContentOffsetKey options:NSKeyValueObservingOptionNew context:NULL];
+            _forceContentOffset = NO;
+        }
+    }
+    return self;
+}
+
+#pragma mark - Collection View
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return _cells.count;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)cv cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewCell *cell = [cv dequeueReusableCellWithReuseIdentifier:kCellReuseID forIndexPath:indexPath];
+    cell.contentView.backgroundColor = indexPath.item % 2 ? [UIColor blueColor] : [UIColor redColor];
+    return cell;
+}
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    return CGSizeMake(kWidth, [_cells[indexPath.item] floatValue]);
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    NSInteger currentHeight = [_cells[indexPath.item] integerValue];
+    
+    BOOL expand = currentHeight == kCollapsedHeight;
+    
+    NSInteger newHeight = expand ? kExpandedHeight : kCollapsedHeight;
+    
+    _cells[indexPath.item] = @(newHeight);
+    
+    __block CGRect frameUpdate = collectionView.frame;
+    _originalOffset = collectionView.contentOffset;
+    
+    if (kHackEnabled) {
+        frameUpdate.size.height += kExpandedHeight - kCollapsedHeight;
+        collectionView.frame = frameUpdate;
+        collectionView.contentOffset = _originalOffset;
+        _forceContentOffset = YES;
+    }
+    
+    [collectionView performBatchUpdates:^{
+        UICollectionViewCell *cell = [collectionView cellForItemAtIndexPath:indexPath];
+        CGRect frame = cell.frame;
+        [UIView animateWithDuration:0.3 animations:^{
+            [cell setFrame:CGRectMake(frame.origin.x, frame.origin.y, frame.size.width, newHeight)];
+        }];
+    } completion:^(BOOL finished) {
+        if (kHackEnabled) {
+            frameUpdate.size.height -= kExpandedHeight - kCollapsedHeight;
+            collectionView.frame = frameUpdate;
+            _forceContentOffset = NO;
+        }
+    }];
+}
+
+#pragma mark - KVO
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    if ([keyPath isEqualToString:kContentOffsetKey] && _forceContentOffset && [(UIScrollView *)object contentOffset].y != _originalOffset.y) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            UICollectionView *collectionView = (UICollectionView *)object;
+            if (collectionView.contentSize.height < (collectionView.contentOffset.y + collectionView.bounds.size.height)) {
+                _forceContentOffset = NO;
+                [collectionView scrollRectToVisible:CGRectMake(_originalOffset.x, collectionView.contentSize.height - collectionView.bounds.size.height, 1, 1) animated:YES];
+            } else {
+                collectionView.contentOffset = _originalOffset;
+            }
+        });
+    }
+}
+
+@end
